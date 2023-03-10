06dea3ea271fdd6c91b1ea11a00bdf1f2bc744d2
Ashutosh Kumar <53196209+TiwariAshuism@users.noreply.github.com>
2023-03-10 23:15:45 +0530

    Sql

--- Day2.sql
+++ Day2.sql
@@ -19,7 +19,7 @@ select category, sum(profit) as total_profit, min(order_date) as First_order, ma
 
 
 
-/* - create the exams table with below script;
+/* - create the exams table with below script;*/
 create table exams (student_id int, subject varchar(20), marks int);
 
 insert into exams values (1,'Chemistry',91),(1,'Physics',91),(1,'Maths',92)
@@ -28,26 +28,35 @@ insert into exams values (1,'Chemistry',91),(1,'Physics',91),(1,'Maths',92)
 ,(4,'Chemistry',71),(4,'Physics',54)
 ,(5,'Chemistry',79);
 
-write a query to find students who have got same marks in Physics and Chemistry.
+/*write a query to find students who have got same marks in Physics and Chemistry.*/
+
 
 select student_id , marks
 from exams
 where subject in ('Physics','Chemistry')
 group by student_id , marks
-having count(1)=2 */
-
+having count(1)=2
 
 
 
 /* write a query to find total number of products in each category. */
 
-
-select * from orders;
+select category,count(distinct product_id) as no_of_products
+from orders
+group by category
 
 
 /* write a query to find top 5 sub categories in west region by total quantity sold */
 
 
-
+select top 5  sub_category, sum(quantity) as total_quantity
+from orders
+where region='West'
+group by sub_category
+order by total_quantity desc
 
 /*write a query to find total sales for each region and ship mode combination for orders in year 2020 */
+select region,ship_mode ,sum(sales) as total_sales
+from orders
+where order_date between '2020-01-01' and '2020-12-31'
+group by region,ship_mode
